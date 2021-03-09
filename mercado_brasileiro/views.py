5b0170870354a47cc98c709d2a42077167952e9b
Ethan Vizitei <evizitei@instructure.com>
2021-03-09 08:44:25 -0600

    show some un-secured database access

--- mercado_brasileiro/views.py
+++ mercado_brasileiro/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Product
 
 def index(request):
     return HttpResponse("Hello, world. You're at the mercado-brasileiro app.")
+
+def products_index(request):
+    products_list = Product.objects.order_by("id")[:10]
+    template = loader.get_template('products/index.html')
+    context = { 'products_list': products_list }
+    return HttpResponse(template.render(context, request))
+
+def products_show(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    template = loader.get_template('products/show.html')
+    context = { 'product': product }
+    return HttpResponse(template.render(context, request))
