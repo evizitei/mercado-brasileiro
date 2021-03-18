a8377dbd4190e41d018b602b0f143546812e20c9
Ethan Vizitei <evizitei@instructure.com>
2021-03-18 15:30:26 -0500

    WIP profile progress from login

--- mercado_brasileiro/forms.py
+++ mercado_brasileiro/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from .models import Product, Seller
 
 class RegistrationForm(forms.Form):
   name = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control" }), label='username')
@@ -6,3 +8,18 @@ class RegistrationForm(forms.Form):
   pw = forms.CharField(widget=forms.PasswordInput(attrs={'class': "form-control" }), label='password')
   pwc = forms.CharField(widget=forms.PasswordInput(attrs={'class': "form-control" }), label='password confirmation')
   seller_uuid = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control" }),label='seller_uuid')
+
+  def clean(self):
+    cleaned_data = super().clean()
+    if cleaned_data.get("pw") != cleaned_data.get("pwc"):
+      raise ValidationError("password and confirmation must match")
+    uuid = cleaned_data.get("seller_uuid")
+    try:
+      seller = Seller.objects.get(seller_uuid=uuid)
+    except ObjectDoesNotExist:
+      raise ValidationError("must provide a valid seller ID")
+
+
+class LoginForm(forms.Form):
+  username = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control" }), label='username')
+  password = forms.CharField(widget=forms.PasswordInput(attrs={'class': "form-control" }), label='password')
--- mercado_brasileiro/views.py
+++ mercado_brasileiro/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import authenticate, logout
 from django.contrib.auth.models import User
+from django.db import transaction
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.template import loader
 
-from .models import Product
-from .forms import RegistrationForm
+from .models import Product, Seller, SellerUser
+from .forms import RegistrationForm, LoginForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at the mercado-brasileiro app.")
@@ -22,16 +25,70 @@ def products_show(request, product_id):
 
 
 def sellers_register(request):
+    return render_registration_form(request, RegistrationForm())
+
+def sellers_login(request):
+    form = LoginForm()
+    return render_login_form(request, form)
+
+def sellers_authenticate(request):
+    if request.method != 'POST':
+        return redirect("sellers_login")
+    form = LoginForm(request.POST)
+    print("FORM DATA: ", form.data)
+    if not form.is_valid():
+        return render_login_form(request, form)
+    username = form.cleaned_data['username']
+    password = form.cleaned_data['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return render_login_form(request, form)
+    else:
+        return redirect("sellers_profile")
+
+def sellers_logout(request):
+    logout(request)
+    redirect("sellers_login")
+
+def sellers_profile(request):
+    if not request.user.is_authenticated:
+        return redirect("sellers_login")
+    seller_user = SellerUser.objects.get(user_id=request.user.id)
+    seller = Seller.objects.get(seller_uuid=seller_user.seller_uuid)
+    template = loader.get_template('sellers/profile.html')
+    context = {'seller': form, 'user': request.user}
+    return HttpResponse(template.render(context, request))
+
+
+def sellers_attach_user(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RegistrationForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            seller_uuid = form.cleaned_data["seller_uuid"]
+            seller = Seller.objects.get(seller_uuid=seller_uuid)
+            uname = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            pw = form.cleaned_data['pw']
+            with transaction.atomic():
+                user = User.objects.create_user(form.cleaned_data, email, pw)
+                user.save()
+                seller_user = SellerUser(
+                    seller_uuid=seller.seller_uuid,
+                    user_id=user.id
+                )
+                seller_user.save(force_insert=True)
+            return redirect("sellers_login")
+        else:
+            return render_registration_form(request, form)
+
+def render_registration_form(request, form):
     template = loader.get_template('sellers/register.html')
-    form = RegistrationForm()
     context = {'form': form}
     return HttpResponse(template.render(context, request))
 
-def sellers_attach_user(request):
-    # Create user and save to the database
-    user = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
-
-    # Update fields and then save again
-    user.first_name = 'John'
-    user.last_name = 'Citizen'
-    user.save()
+def render_login_form(request, form):
+    template = loader.get_template('sellers/login.html')
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
