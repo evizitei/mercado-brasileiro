30f7752566fad7320075dd5f63fa4c2976ce8ff4
Ethan Vizitei <evizitei@instructure.com>
2021-03-29 11:46:05 -0500

    add product search by category name

--- mercado_brasileiro/urls.py
+++ mercado_brasileiro/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('', views.products_index, name='index'),
     path('admin/', admin.site.urls),
     path('products/', views.products_index, name="products_index"),
+    path('products/search', views.products_search, name="products_search"),
     path('products/<int:product_id>', views.products_show, name="product_show"),
     path('sellers/register', views.sellers_register, name="sellers_register"),
     path('sellers/attach', views.sellers_attach_user, name="sellers_attach_user"),
